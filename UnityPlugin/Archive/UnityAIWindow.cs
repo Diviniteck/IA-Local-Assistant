20827fe343855dfe986554f5ad809e11beaed41b
Diviniteck <mathias.venault@gmail.com>
2026-03-23 17:57:31 +0100

    Delete UnityPlugin/Archive directory

--- UnityPlugin/Archive/UnityAIWindow.cs
+++
@@ -1,5 +0,0 @@
-// Ancien système de fenêtre Unity archivé.
-// Cette version V2 utilise désormais une application Python principale
-// + un bridge Unity dédié.
-//
-// Fichier laissé vide volontairement pour éviter les erreurs de compilation.
